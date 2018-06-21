17d9e844fda0fe2f88501837912edea8a3c20833
VPayonk <v.payonk@gmail.com>
2018-06-21 16:08:36 +0300

    first commit

---
+++ Vue1/js/js.js
@@ -0,0 +1,22 @@
+
+// function min(a, b) {
+//     var a,b;
+//     return (a > b) ? b :a;
+// }
+//
+// function pow(x,n) {
+//     var x,n,pow;
+//     pow = x;
+//     for (var i=1; i < n; i++ ) {
+//         pow *= x;
+//     }
+//     return pow;
+// }
+//
+//
+// var animal = {
+//   name: "Васька",
+//   canWalk: true
+// }
+
+
