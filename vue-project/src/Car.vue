735d9da5c57f2096cedc2e48d3ee1fc617cc2b00
VPayonk <v.payonk@gmail.com>
2018-06-25 15:02:30 +0300

    Local and global component in Vue-project

---
+++ vue-project/src/Car.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>
+            {{ carName }} : {{ carYear }}
+        </h3>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        carName: 'Ford',
+        carYear: 2015
+      }
+    }
+  };
+</script>
+
+<style lang="scss">
+
+</style>
