803cbbb2061c9acf942aad5eed46c8474472225e
VPayonk <v.payonk@gmail.com>
2018-06-25 14:59:00 +0300

    Local and global component in Vue-project

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// global component
+// import Car from './Car.vue'
+//
+// Vue.component('app-car', Car)
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
